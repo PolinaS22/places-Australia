4a93f235d351dd8afee046e4a63241842ac38c3d
PolinaS22 <shishkinapln@yandex.ru>
2024-07-29 13:39:13 +0300

    updated

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import './places.css'
 
 export default function Main() {
 
     return (
-        <div>
-            <p>Welcome. Choose your destination:</p>
-            <button><Link to="/melbourne">Melbourne</Link></button>
-            <button><Link to="/sydney">Sydney</Link></button>
-            <button><Link to="/perth">Perth</Link></button>
-            
-
+        <div className="bodyMain">
+            <div className="titleMain">
+                <p>Welcome. Choose your destination:</p>
+            </div>
+           <div className='containerMain'>
+                <div className="btnDestination btnMelb">
+                    <Link className="linkMain" to="/melbourne">Melbourne</Link>
+                </div>
+                <div className="btnDestination btnSyd">
+                    <Link className="linkMain" to="/sydney">Sydney</Link>
+                </div>
+                <div className="btnDestination btnPer">
+                    <Link className="linkMain" to="/perth">Perth</Link>
+                </div>
+           </div>
         </div>
     )
 }
